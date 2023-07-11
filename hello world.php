aeff04660e49293515763e50449094fea7871267
Slam-James <129076048+Slam-James@users.noreply.github.com>
2023-07-11 15:18:55 -0400

    first commit

---
+++ hello world.php
@@ -0,0 +1,5 @@
+<?php
+echo"hello world \n...\n...\n...\n";
+echo"This is a computer program crreated by a grade 11 student using PHP! \n";
+echo"Welcome to this program, and have a great day.";
+?>
