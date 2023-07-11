2e5e973bb4cb64b9504d4f465d175a4d6a9a34d1
Slam-James <129076048+Slam-James@users.noreply.github.com>
2023-07-11 15:27:14 -0400

    Final version of hello world

--- hello world.php
+++ hello world.php
@@ -2,4 +2,5 @@
 echo"hello world \n...\n...\n...\n";
 echo"This is a computer program crreated by a grade 11 student using PHP! \n";
 echo"Welcome to this program, and have a great day.";
+echo" Goodbye Now :)";
 ?>
